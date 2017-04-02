ab096886dc51be5552a5bc6fcc16ab2ac5e00e21
samuel <berriens@gmail.com>
2017-04-02 16:39:36 +0200

    heightMap -> tj crash quand pas sur emulateur...

--- app/src/main/java/com/samuelberrien/spectrix/obj/visualization/TestSpecular.java
+++ app/src/main/java/com/samuelberrien/spectrix/obj/visualization/TestSpecular.java
@@ -30,7 +30,7 @@ public class TestSpecular {
         this.mPingTranslateVector = new float[]{0f, -2f, 0f};
         this.mPingRotationMatrix = new float[16];
         this.mPingModelMatrix = new float[16];*/
-        this.hm = new HeightMap(context, R.drawable.mountains_height_1, R.drawable.mountains_tex_1, 0.1f, 0.5f);
+        this.hm = new HeightMap(context, R.drawable.mountains_height_2, R.drawable.mountains_tex_2, 0.1f, 0.8f);
         this.mHmModelMatrix = new float[16];
     }
 
--- app/src/main/res/raw/fragment_shader_height_map.glsl
+++ app/src/main/res/raw/fragment_shader_height_map.glsl
@@ -13,18 +13,7 @@ uniform sampler2D textureMap;
 void main () {
     float distance = length(u_LightPos - v_Position);
     vec3 lightVector = normalize(u_LightPos - v_Position);
-
     float diffuse = max(0.2, dot(normal, lightVector)) * u_light_coef;
-    //vec4 frag_colour = (0.05 - newVp.z) * vec4(1.0, 1.0, 0.0, 1.0) + newVp.z * vec4(1.0, 0.25, 0.0, 1.0);
     vec4 frag_colour = texture2D(textureMap, newVp.xz);
-
-    /*if(z <= 0.3){
-        float alpha = z / 0.3;
-        frag_colour = (1.0 - alpha) * vec4(1.0, 1.0, 0.0, 1.0) + alpha * vec4(1.0, 0.5, 0.25, 1.0);
-     } else {
-        float alpha = min((z - 0.3) / 0.7, 1.0);
-        frag_colour = (1.0 - alpha) * vec4(1.0, 0.5, 0.25, 1.0) + alpha * vec4(1.0, 0.25, 0.0, 1.0);
-    }*/
-
     gl_FragColor = vec4(frag_colour.rgb * diffuse, 1.0);
 }
